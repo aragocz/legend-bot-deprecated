8216fb71cc77a195872d5d17ae54cc9687041f46
ShadowLegend <kubuleulule@email.cz>
2020-11-25 13:32:13 +0100

    Purge repair

--- Commands/Purge.js
+++ Commands/Purge.js
@@ -1,10 +1,11 @@
 module.exports = {
     name: 'purge',
-    description: "Deletes 1 - 99 messages.",
+    description: "Deletes 1 - 99 messages. + 1 aka the message you sent.",
     execute(message, args){
         if(message.deletable) message.delete
-        const amountdef = args
-        const amount = Math.floor(amountdef + 1)
+        let amountdef = args
+        let amountdef2 = Math.floor(amountdef)
+        let amount = amountdef2 + 1
         if(!message.member.hasPermission("MANAGE_MESSAGES")){
             message.channel.send(`You need permission \`MANAGE_MESSAGES\` to purge messages!`).then(m => m.delete({timeout: 10000}))
         }else {
@@ -15,7 +16,7 @@ module.exports = {
                     message.channel.send("The number must be between 1 - 99.").then(m => m.delete({timeout: 10000}))
                 }else {
                     message.channel.bulkDelete(amount , true)
-                    message.channel.send(`Succesfully removed ${amountdef} messages!`).then(m => m.delete({timeout: 10000}))
+                    message.channel.send(`Succesfully removed ${amountdef2} messages!`).then(m => m.delete({timeout: 10000}))
                 }
             }
         }
