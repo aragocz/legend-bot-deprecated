8d3c1ab3a54e1dde1383ad2317ed988fe3253df4
ShadowLegend <kubuleulule@email.cz>
2021-01-25 18:44:21 +0100

    Hug [BETA]

--- index.js
+++ index.js
@@ -244,6 +244,25 @@ bot.on("message" , async message => {
     if(cmd === `${prefix}unvm` || cmd === `${prefix}unvoicemute`){
         bot.commands.get('unvoicemute').execute(message, args)
     }
+
+    if(cmd === `${prefix}hug`){
+        const muser = message.mentions.users.first();
+        const embed = new Discord.MessageEmbed();
+        const expgifs = [
+            "https://media.tenor.com/images/ca88f916b116711c60bb23b8eb608694/tenor.gif",
+            "https://media.tenor.com/images/778282e02d511fbc061e1439a5105c6f/tenor.gif",
+            "https://media.tenor.com/images/ca682cecd6bff521e400f984502f370c/tenor.gif",
+            "https://media.tenor.com/images/bb67bef5f54d0191b7e2d3c1fd6e4bd3/tenor.gif",
+            "https://media.tenor.com/images/d7f6849b07da0532c7dc3aab538d42d4/tenor.gif",
+            "https://media.tenor.com/images/9fe95432f2d10d7de2e279d5c10b9b51/tenor.gif",
+            "https://media.tenor.com/images/043d27b3f7715ac7c2eb6d8670b14336/tenor.gif"
+        ]
+        let gif = expgifs[Math.floor(Math.random() * expgifs.length)];
+        embed.setTitle(`${message.author.username} hugs ${muser.username}`)
+        embed.setImage(gif);
+
+        message.channel.send(embed)
+    }
 })
 
 
