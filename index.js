0b14fc600975958bcccb3fba9ca8967665b2be66
ShadowLegend <kubuleulule@email.cz>
2021-01-26 11:05:39 +0100

    More GIFs in hu command

--- index.js
+++ index.js
@@ -255,7 +255,11 @@ bot.on("message" , async message => {
             "https://media.tenor.com/images/bb67bef5f54d0191b7e2d3c1fd6e4bd3/tenor.gif",
             "https://media.tenor.com/images/d7f6849b07da0532c7dc3aab538d42d4/tenor.gif",
             "https://media.tenor.com/images/9fe95432f2d10d7de2e279d5c10b9b51/tenor.gif",
-            "https://media.tenor.com/images/043d27b3f7715ac7c2eb6d8670b14336/tenor.gif"
+            "https://media.tenor.com/images/043d27b3f7715ac7c2eb6d8670b14336/tenor.gif",
+            "https://media.tenor.com/images/ea1ca14e49866429e2221aab2126cdb0/tenor.gif",
+            "https://gifimage.net/wp-content/uploads/2017/06/anime-hug-gif-12.gif",
+            "https://media.tenor.com/images/1171c186f9130d1efa4a186ad4371e8c/tenor.gif",
+            "http://media.tenor.com/images/f08f77fc242b9a8263206caa42795d9d/tenor.gif"
         ]
         let gif = expgifs[Math.floor(Math.random() * expgifs.length)];
         embed.setTitle(`${message.author.username} hugs ${muser.username}`)
