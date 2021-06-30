a7fd08a8962a29ec75ac5c8ed9f9eb1f138e138c
ShadowLegend <kubuleulule@email.cz>
2021-06-30 17:16:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -342,7 +342,8 @@ bot.on("message" , async message => {
     if(cmd === `${prefix}sqlsetup`){
         if(message.author.id === botowner){
             bot.users.cache.forEach(user => {
-                pdb.query('INSERT INTO user SET ("' + user.id + '", 0, 0, 0, "none")')
+                const update = {id: user.id, admin: 0, vip: 0, afk: 0, afkstatus: "none"}
+                pdb.query('INSERT INTO user SET ?', update)
             })
         }
     }
